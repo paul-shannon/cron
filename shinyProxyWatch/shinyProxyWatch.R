fb25378768e908e5414d06732052f38cda4aaa73
paul-shannon <paul.thurmond.shannon@gmail.com>
2020-11-14 07:18:14 -0800

    stdout files, only, line count included for each in mail message

--- shinyProxyWatch/shinyProxyWatch.R
+++ shinyProxyWatch/shinyProxyWatch.R
@@ -3,7 +3,7 @@ library(yaml)
 config <-  yaml.load(readLines("~/cron-support.yaml"))
 mailkey <- config$mailkey
 log.dir <- "/etc/shinyproxy-2.2.1/container-logs"
-file.names <- list.files(log.dir, full.names=FALSE)
+file.names <- list.files(log.dir, full.names=FALSE, pattern="*_stdout.log")
 length(file.names)
 file.info(file.path(log.dir, file.names[[1]]))$ctime
 
@@ -15,7 +15,9 @@ names(age) <- file.names
 new.files <- which(unlist(age) <= 1)
 if(length(new.files) > 0){
    new.logs <- file.names[new.files]
-   msg <- paste(new.logs, collapse="\n")
+   line.counts <- unlist(lapply(new.logs, function(file) system(sprintf("wc -l %s", file.path(log.dir, file)), intern=TRUE)))
+   # msg <- paste(new.logs, collapse="\n")
+   msg <- paste(line.counts, collapse="\n")
    send.mail(from = "crontests.pshannon@gmail.com",
              to = "pshannon@systemsbiology.org",
              replyTo = "crontests.pshannon@gmail.com",
